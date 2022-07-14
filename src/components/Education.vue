b1865ae6c5f99f8ae614bd50590d2f9042ff50b5
Holger Mueller <9jholger@gmail.com>
2022-07-14 14:55:08 -0400

    added info to education section

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -1,12 +1,56 @@
 <template>
   <div id="Education">
     <h1 class="section-header">Education</h1>
+    <div v-for="(item, index) in education" :key="index">
+      <v-card class="education-card" tile>
+        <v-card-title>
+          {{ item.school }}
+        </v-card-title>
+        <v-card-text>
+          {{ item.program }}
+        </v-card-text>
+      </v-card>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "Education",
+
+  data: () => ({
+    education: [
+      {
+        school: "freeCodeCamp",
+        program: "Javascript Algorithms and Data Structures",
+      },
+      {
+        school: "Wes Bos",
+        program: "JavaScript30",
+      },
+      {
+        school: "University of Minnesota College of Continuing Education",
+        program: "Full-stack Web Developer Certificate",
+      },
+      {
+        school: "SUNY New Paltz",
+        program: "Master's English",
+      },
+      {
+        school: "SUNY New Paltz",
+        program: "Bachelor's Creative Writing",
+      },
+    ],
+  }),
+};
+</script>
 
 <style scoped>
 .section-header {
   text-align: center;
-  margin-top: 7%;
+}
+.education-card {
+  width: 55%;
+  margin: 1px auto;
 }
 </style>
--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -44,73 +44,71 @@
 export default {
   name: "Portfolio",
 
-  data() {
-    return {
-      Projects: [
-        {
-          title: "Trip Scheduler",
-          description: "A way to keep track of any upcoming getaways.",
-          image: require("../images/scheduler.png"),
-          repolink: "https://github.com/Holgermueller/vue-train-scheduler",
-          tech: "Vue, Vuex, Vuetify, Cloud Firestore",
-          demoLink: "https://moodtracker-db661.web.app/?redirect=%2Fdashboard",
-        },
-        {
-          title: "Hours Tracker",
-          description:
-            "A way for me to track hours I need to make up due to the bookstore closing early.",
-          image: require("../images/hoursTracker.png"),
-          repolink: "https://github.com/Holgermueller/vue-hours-tracker",
-          tech: "Vue, Vuex, Vuetify, Cloud Firestore",
-          demoLink: "https://trackmyhours-74a8f.web.app/",
-        },
-        {
-          title: "Vue Wishlist",
-          description:
-            "Vue front-end and cloud firestore back-end. CRUD application. Gives users an alternative to using Amazon's wishlist as a way of tracking items they'd like to purchase, or have someone purchase for them.",
-          image: require("../images/wishList.png"),
-          repolink: "https://github.com/Holgermueller/wish-list",
-          tech: "Vue, Vuex, Vuetify, Cloud Firestore",
-          demoLink: "https://less-wish-list.web.app/dashboard",
-        },
-        {
-          title: "Story-Idea Generator",
-          description:
-            "Single page application that helps stumped writers come up with their next best seller",
-          image: require("../images/storyIdeaGenerator.png"),
-          repolink: "https://github.com/Holgermueller/story-idea-generator",
-          tech: "Gatsby, Material-Ui",
-          demoLink: "https://confident-tesla-1e3118.netlify.app/",
-        },
-        {
-          title: "Time and Temp",
-          description: "A weathr app with location search.",
-          image: require("../images/timetempshot.png"),
-          repolink: "https://github.com/Holgermueller/TimeandTemp",
-          tech: "Handlebars, ExpressJs, LessCSS, Nodemon",
-          demoLink: "https://holgerweatherapp.herokuapp.com/",
-        },
-        {
-          title: "Inspirational Thought Generator",
-          description:
-            "An app that gives users words of widsom to get them through the day. Thoughts can be shared on Twitter.",
-          image: require("../images/inspthghtgnrtr.png"),
-          repolink:
-            "https://github.com/Holgermueller/Inspirational-thought-generator",
-          tech: "jQuery, Grunt, Nodemon",
-          demoLink: "https://inspirationalthoughtgenerator.herokuapp.com/",
-        },
-        {
-          title: "Friend Finder",
-          description: "A friend-finding app/ poor-man's dating app.",
-          image: require("../images/friendfinder.png"),
-          repolink: "https://github.com/Holgermueller/Friend_Finder",
-          tech: "Expressjs, Nodemon, jQuery",
-          demoLink: "https://friendfinderap.herokuapp.com/",
-        },
-      ],
-    };
-  },
+  data: () => ({
+    Projects: [
+      {
+        title: "Trip Scheduler",
+        description: "A way to keep track of any upcoming getaways.",
+        image: require("../images/scheduler.png"),
+        repolink: "https://github.com/Holgermueller/vue-train-scheduler",
+        tech: "Vue, Vuex, Vuetify, Cloud Firestore",
+        demoLink: "https://moodtracker-db661.web.app/?redirect=%2Fdashboard",
+      },
+      {
+        title: "Hours Tracker",
+        description:
+          "A way for me to track hours I need to make up due to the bookstore closing early.",
+        image: require("../images/hoursTracker.png"),
+        repolink: "https://github.com/Holgermueller/vue-hours-tracker",
+        tech: "Vue, Vuex, Vuetify, Cloud Firestore",
+        demoLink: "https://trackmyhours-74a8f.web.app/",
+      },
+      {
+        title: "Vue Wishlist",
+        description:
+          "Vue front-end and cloud firestore back-end. CRUD application. Gives users an alternative to using Amazon's wishlist as a way of tracking items they'd like to purchase, or have someone purchase for them.",
+        image: require("../images/wishList.png"),
+        repolink: "https://github.com/Holgermueller/wish-list",
+        tech: "Vue, Vuex, Vuetify, Cloud Firestore",
+        demoLink: "https://less-wish-list.web.app/dashboard",
+      },
+      {
+        title: "Story-Idea Generator",
+        description:
+          "Single page application that helps stumped writers come up with their next best seller",
+        image: require("../images/storyIdeaGenerator.png"),
+        repolink: "https://github.com/Holgermueller/story-idea-generator",
+        tech: "Gatsby, Material-Ui",
+        demoLink: "https://confident-tesla-1e3118.netlify.app/",
+      },
+      {
+        title: "Time and Temp",
+        description: "A weathr app with location search.",
+        image: require("../images/timetempshot.png"),
+        repolink: "https://github.com/Holgermueller/TimeandTemp",
+        tech: "Handlebars, ExpressJs, LessCSS, Nodemon",
+        demoLink: "https://holgerweatherapp.herokuapp.com/",
+      },
+      {
+        title: "Inspirational Thought Generator",
+        description:
+          "An app that gives users words of widsom to get them through the day. Thoughts can be shared on Twitter.",
+        image: require("../images/inspthghtgnrtr.png"),
+        repolink:
+          "https://github.com/Holgermueller/Inspirational-thought-generator",
+        tech: "jQuery, Grunt, Nodemon",
+        demoLink: "https://inspirationalthoughtgenerator.herokuapp.com/",
+      },
+      {
+        title: "Friend Finder",
+        description: "A friend-finding app/ poor-man's dating app.",
+        image: require("../images/friendfinder.png"),
+        repolink: "https://github.com/Holgermueller/Friend_Finder",
+        tech: "Expressjs, Nodemon, jQuery",
+        demoLink: "https://friendfinderap.herokuapp.com/",
+      },
+    ],
+  }),
 };
 </script>
 
@@ -118,6 +116,7 @@ export default {
 #Portfolio {
   background-color: #8fc1e3;
   width: 100%;
+  padding-bottom: 7%;
 }
 .section-header {
   text-align: center;
