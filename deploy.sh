b4c7591aac1815759c09c2588eb07691575d11ae
Holger Mueller <9jholger@gmail.com>
2022-07-14 12:03:27 -0400

    test push

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd dist
 git init
 git add -A
 git commit -m 'New Deployment'
-git push -f git@github.com:Holgermueller/test-app.git master:gh-pages
+git push -f git@github.com:Holgermueller/portfolio.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/test-app/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/portfolio/" : "/",
 
-  transpileDependencies: [
-    'vuetify'
-  ]
+  transpileDependencies: ["vuetify"],
 };
